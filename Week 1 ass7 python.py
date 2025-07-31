2bbc588d6f15fc590434f72ebab78a5957e07493
Rahul pandit <panditrahul5080@gmail.com>
2025-07-31 20:32:17 +0530

    Reverse a string

---
+++ Week 1 ass7 python.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    return s[::-1]  # Slicing method to reverse string
+
+# ---------- Driver Code ----------
+if __name__ == "__main__":
+    s = input("Enter a string: ")
+    print("Reversed string:", reverse_string(s))
